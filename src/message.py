b233bbc10f250f17e6eadb92171562a3d0b6ed86
robert-min <robertmin522@gmail.com>
2024-07-30 12:43:07 +0900

    test: ADD dynamodb connecter test code

--- src/message.py
+++ src/message.py
@@ -8,7 +8,8 @@ async def get_pong(ctx):
 async def get_gpt_answer(ctx, query):
     if ctx.message.channel.type == discord.ChannelType.public_thread:
         thread_id = ctx.message.channel.id
-        print(f'Thread ID: {thread_id}')
+        print(ctx.message)  # channel > parent
+        print(f'Thread ID: {thread_id}')    # Thread id
         await ctx.send("hello thread!")
     else:
         author = ctx.message.author
@@ -18,6 +19,7 @@ async def get_gpt_answer(ctx, query):
             f"Discriminator: {author.discriminator}\n"
             f"Mention: {author.mention}\n"
         )
+        print(ctx.message)  # channel > name
         print(author_info)
 
         thread = await ctx.message.create_thread(
