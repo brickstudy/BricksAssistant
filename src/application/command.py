0575f6c30d518ddece36e9d9e9a64ca7fe50f147
robert-min <robertmin522@gmail.com>
2024-08-15 12:04:14 +0900

    feature: ADD info 권한, 소개 command.

--- src/application/command.py
+++ src/application/command.py
@@ -1,3 +1,4 @@
+import os
 import discord
 from datetime import datetime
 
@@ -42,3 +43,27 @@ async def get_gpt_answer(ctx, question: str):
 
         result = service.request_answer(client, db, info)
         await ctx.send(result.answer)
+
+
+async def get_info(ctx, arg: str):
+    application_path = os.path.abspath(os.path.join(__file__, os.path.pardir))
+    info_path = os.path.abspath(os.path.join(application_path, "info"))
+    # Check arg
+    if not arg:
+        await ctx.send("사용 가능한 명령어:\n\n!intro 소개\n!intro 권한\n!intro 인프라")
+    elif arg == "소개":
+        try:
+            with open(info_path + "/소개.txt", 'r', encoding="utf-8") as f:
+                content = f.read()
+                await ctx.send(content)
+        except FileNotFoundError:
+            await ctx.send("소개 파일을 찾을 수 없습니다. 관리자에게 문의해주세요.")
+    elif arg == "권한":
+        try:
+            with open(info_path + "/권한.txt", 'r', encoding="utf-8") as f:
+                content = f.read()
+                await ctx.send(content)
+        except FileNotFoundError:
+            await ctx.send("권한 파일을 찾을 수 없습니다. 관리자에게 문의해주세요.")
+    else:
+        await ctx.send("잘못된 명령어입니다. !intro 만 입력하여 사용 가능한 명령어를 확인하세요.")
